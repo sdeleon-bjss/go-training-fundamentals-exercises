63d539ff2a8003ebd4b48e000d0cf15d63b04d69
Steven DeLeon <Steven.DeLeon@bjss.com>
2024-03-05 11:19:12 -0500

    exercise 7 - dice roll and tests

---
+++ assessments/fundamentals/07_dice_roller/dice_roller_test.go
@@ -0,0 +1,55 @@
+package main_test
+
+import (
+	pkg "bjss-go-training/assessments/fundamentals/07_dice_roller"
+	"testing"
+)
+
+type TestRoll struct {
+	roll     int
+	expected string
+}
+
+func TestProcessRoll(t *testing.T) {
+	testCases := []TestRoll{
+		{roll: 1, expected: "NEUTRAL"},
+		{roll: 2, expected: "SNAKE-EYES-CRAPS"},
+		{roll: 3, expected: "LOSS-CRAPS"},
+		{roll: 4, expected: "NEUTRAL"},
+		{roll: 5, expected: "NEUTRAL"},
+		{roll: 6, expected: "NEUTRAL"},
+		{roll: 7, expected: "NATURAL"},
+		{roll: 8, expected: "NEUTRAL"},
+		{roll: 9, expected: "NEUTRAL"},
+		{roll: 10, expected: "NEUTRAL"},
+		{roll: 11, expected: "NATURAL"},
+		{roll: 12, expected: "LOSS-CRAPS"},
+	}
+
+	for _, test := range testCases {
+		result := pkg.ProcessRoll(test.roll)
+		if result != test.expected {
+			t.Errorf("Expected %s but got %s", test.expected, result)
+		}
+	}
+}
+
+func TestRollDice(t *testing.T) {
+	diceRoll := pkg.RollDice()
+
+	if diceRoll < 1 || diceRoll > 6 {
+		t.Errorf("Expected a number between 1 and 6 but got %d", diceRoll)
+	}
+}
+
+func TestComboRolls(t *testing.T) {
+	result := pkg.ComboRolls()
+
+	if result[0] < 1 || result[0] > 6 {
+		t.Errorf("Expected a number between 1 and 6 but got %d", result[0])
+	}
+
+	if result[1] < 1 || result[1] > 6 {
+		t.Errorf("Expected a number between 1 and 6 but got %d", result[1])
+	}
+}
--- assessments/fundamentals/07_dice_roller/main.go
+++ assessments/fundamentals/07_dice_roller/main.go
@@ -1,3 +1,43 @@
 package main
 
-func main() {}
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Create a program that rolls two dice (1 to 6) fifty times. Display the number rolls and the outcomes in sequential order.
+// Resulting rolls are to be processed in the following manner: [Random Numbers][Switches]
+// - 7 and 11 are to be called NATURAL
+// - 2 is called SNAKE-EYES-CRAPS
+// - 3 and 12 is called LOSS-CRAPS
+// - Any other combination is called NEUTRAL.
+func main() {
+	for i := 0; i < 50; i++ {
+		rolls := ComboRolls()
+		combinedRolls := rolls[0] + rolls[1]
+		rollResult := ProcessRoll(combinedRolls)
+		output := fmt.Sprintf("Roll attempt #%d: You rolled a (%d) = %s", i+1, combinedRolls, rollResult)
+		fmt.Println(output)
+	}
+}
+
+func ComboRolls() [2]int {
+	return [2]int{RollDice(), RollDice()}
+}
+
+func RollDice() int {
+	return rand.Intn(6) + 1
+}
+
+func ProcessRoll(roll int) string {
+	switch roll {
+	case 7, 11:
+		return "NATURAL"
+	case 2:
+		return "SNAKE-EYES-CRAPS"
+	case 3, 12:
+		return "LOSS-CRAPS"
+	default:
+		return "NEUTRAL"
+	}
+}
